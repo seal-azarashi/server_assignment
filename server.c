affc95509dfad3e1c142c23cd5cb5419030b7f8e
seal_azarashi <azarashi@example.com>
2024-10-08 09:12:31 +0900

    accept ipv6 & ipv4

--- server.c
+++ server.c
@@ -6,18 +6,23 @@
 
 int main()
 {
-    struct sockaddr_in socket_address;
-    socket_address.sin_family = AF_INET;
-    socket_address.sin_addr.s_addr = inet_addr(LOOPBACK_ADDRESS);
-    socket_address.sin_port = htons(PORT);
+    struct sockaddr_in6 socket_address;
+    socket_address.sin6_family = AF_INET6;
+    socket_address.sin6_addr = in6addr_any;
+    socket_address.sin6_port = htons(PORT);
 
     int address_length = sizeof(socket_address);
     char buffer[BUFFER_SIZE] = {0};
     char *http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
     ssize_t bytes_read, bytes_written;
 
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
     check_error(socket_fd == -1, "socket failed", socket_fd);
+
+    int ipv6only = 0;
+    int setsockopt_result = setsockopt(socket_fd, IPPROTO_IPV6, IPV6_V6ONLY, &ipv6only, sizeof(ipv6only));
+    check_error(setsockopt_result == -1, "setsockopt failed", socket_fd);
+
     int bind_result = bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address));
     check_error(bind_result == -1, "bind failed", socket_fd);
 
@@ -25,7 +30,7 @@ int main()
     int listen_result = listen(socket_fd, 3);
     check_error(listen_result == -1, "listen failed", socket_fd);
 
-    printf("Server listening on %s, port %d\n", LOOPBACK_ADDRESS, PORT);
+    printf("Server listening on port %d (IPv4 and IPv6)\n", PORT);
 
     while (1)
     {
